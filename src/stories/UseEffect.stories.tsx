65ff6c857f47c05e7d7790b3dde96c3b9a000796
lorem <79251934+adisovaa@users.noreply.github.com>
2023-06-20 22:06:41 -0400

    22 - useEffect \ setTimeout \ setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 
 export const UseEffectStoriesJsx = () => {
+    const [fake, setFake] = useState(0)
     const [counter, setCounter] = useState(0)
 
     useEffect(() => {
@@ -14,29 +15,55 @@ export const UseEffectStoriesJsx = () => {
     })
 
     useEffect(() => {
-        //API
-        //setInterval
-        //indexDB
-        //document.getElementByD
-        //document.title = 'User'
         console.log('useEffect only in first render')
         document.title = counter.toString()
     }, [])
 
     useEffect(() => {
-        //API
-        //setInterval
-        //indexDB
-        //document.getElementByD
-        //document.title = 'User'
         console.log('useEffect in first render and every counter change')
         document.title = counter.toString()
     }, [counter])
 
+    useEffect(() => {
+        setTimeout(() => {
+            document.title = counter.toString()
+        }, 1000)
+    }, [])
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter((state) => state + 1)
+        }, 1000)
+    }, [])
+
 
     return (
         <div>
-            <button onClick={() => setCounter(counter + 1)}>+</button>
+            <button onClick={() => setFake(fake + 1)}>fake +</button>
+            <button onClick={() => setCounter(counter + 1)}>counter +</button>
         </div>
     );
 };
+
+
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(1)
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         console.log('useEffect every render')
+    //         document.title = counter.toString()
+    //     }, 1000)
+    // }, [counter])
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter((state) => state + 1)
+        }, 1000)
+    }, [])
+
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>counter + 1</button>
+    </>
+}
